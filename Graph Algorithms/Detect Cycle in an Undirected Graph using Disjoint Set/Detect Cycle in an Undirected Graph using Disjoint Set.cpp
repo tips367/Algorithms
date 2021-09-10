2f1dd7cbe9d94bc952581ca56dfe0cdf43f66e85
tips367 <tipu367.khan@hotmail.com>
2021-09-10 17:34:29 +0530

    Optimized using union by rank and path compression

--- Graph Algorithms/Detect Cycle in an Undirected Graph using Disjoint Set/Detect Cycle in an Undirected Graph using Disjoint Set.cpp
+++ Graph Algorithms/Detect Cycle in an Undirected Graph using Disjoint Set/Detect Cycle in an Undirected Graph using Disjoint Set.cpp
@@ -26,6 +26,9 @@ Graph* createGraph(int V, int E)
     return graph;
 }
 
+// Method 1 : Simple
+
+/*
 // A utility function to find the subset of an element i
 int find(int parent[], int i)
 {
@@ -47,7 +50,7 @@ bool isCycle(Graph* graph)
     // Initialize all subsets as single element sets
     memset(parent, -1, sizeof(int) * graph->V);
 
-    // Iterate through all edges of graph, find subset ofboth vertices of every edge, if both subsets are
+    // Iterate through all edges of graph, find subset of both vertices of every edge, if both subsets are
     // same, then there is cycle in graph.
     for (int i = 0; i < graph->E; i++) 
     {
@@ -58,6 +61,66 @@ bool isCycle(Graph* graph)
             return true;
 
         Union(parent, x, y);
+    }
+    return false;
+} */
+
+// Method 2 : Optimized using union by rank and path compression
+class subset 
+{
+public:
+    int parent;
+    int rank;
+};
+
+int find(subset subsets[], int i)
+{
+    // find root and make root as parent of i (path compression)
+    if (subsets[i].parent == -1)
+        return i;
+       
+    return subsets[i].parent = find(subsets, subsets[i].parent);
+}
+
+void Union(subset subsets[], int xroot, int yroot)
+{
+    // Attach smaller rank tree under root of high rank tree (Union by Rank)
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    // If ranks are same, then make one as root and increment its rank by one
+    else 
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+bool isCycle(Graph* graph)
+{
+    int V = graph->V;
+    int E = graph->E;
+
+    subset* subsets = new subset[V];
+
+    for (int v = 0; v < V; ++v) 
+    {
+        subsets[v].parent = -1;
+        subsets[v].rank = 0;
+    }
+
+    // Iterate through all edges of graph, find sets of both vertices of every edge, if sets are same, then there
+    // is cycle in graph.
+    for (int e = 0; e < E; ++e) 
+    {
+        int x = find(subsets, graph->edge[e].src);
+        int y = find(subsets, graph->edge[e].dest);
+
+        if (x == y)
+            return true;
+
+        Union(subsets, x, y);
     }
     return false;
 }
