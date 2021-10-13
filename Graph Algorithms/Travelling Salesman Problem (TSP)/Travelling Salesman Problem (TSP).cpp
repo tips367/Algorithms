7546e19be1746b87baf6f45c816d818fd2206ceb
tips367 <tipu367.khan@hotmail.com>
2021-10-13 23:56:06 +0530

    TSP using dynamic programming

--- Graph Algorithms/Travelling Salesman Problem (TSP)/Travelling Salesman Problem (TSP).cpp
+++ Graph Algorithms/Travelling Salesman Problem (TSP)/Travelling Salesman Problem (TSP).cpp
@@ -6,7 +6,10 @@
 
 #define V 4
 
+int VISITED_ALL = (1 << V) - 1;
+
 // Method 1 : Naive...Time complexity : O(n!)
+/*
 int travellingSalesmanProblem(int graph[][V], int s)
 {
     // store all vertex apart from source vertex
@@ -44,6 +47,41 @@ int main()
     int s = 0;
     std::cout << travellingSalesmanProblem(graph, s) << std::endl;
     return 0;
+} */
+
+// Method 2 : Using Dynamic programming...Time complexity : O(n^2*2^n)
+int travellingSalesmanProblem(int graph[][V], int mask, int pos, std::vector<std::vector<int>>& dp)
+{
+    if (mask == VISITED_ALL)
+        return graph[pos][0];
+
+    if (dp[mask][pos] != -1)
+        return dp[mask][pos];
+
+    //Now from current node, we will try to go to every other node and take the min ans
+    int ans = INT_MAX;
+
+    //Visit all the unvisited cities and take the best route
+    for (int city = 0; city < V; city++)
+    {
+        if ((mask & (1 << city)) == 0)
+        {
+            int newAns = graph[pos][city] + travellingSalesmanProblem(graph, mask | (1 << city), city, dp);
+            ans = std::min(ans, newAns);
+        }
+    }
+    return dp[mask][pos] = ans;
 }
 
+int main()
+{
+    // matrix representation of graph
+    int graph[][V] = {{0, 20, 42, 25},
+                      {20, 0, 30, 34},
+                      {42, 30, 0, 10},
+                      {25, 34, 10, 0}};
 
+    std::vector<std::vector<int>> dp(V * V, std::vector<int>(V, -1));
+    std::cout << travellingSalesmanProblem(graph, 1, 0, dp) << std::endl;
+    return 0;
+}
